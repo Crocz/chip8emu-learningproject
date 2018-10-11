5e2510e7dd8d5c434d32856b2866d335535c447d
Richard Nordström <croczter@gmail.com>
2018-10-11 20:21:51 +0200

    Added in skeleton in Chip8Emu. Added RegisterNames (still not in use).

--- chip8core/Chip8Emu.cs
+++ chip8core/Chip8Emu.cs
@@ -1,15 +1,48 @@
 ﻿using System;
 
-namespace chip8core
-{
-    public class Chip8Emu
-    {        
-        //memory area
-        ///program counter
-        ///
+namespace chip8core {
+    public sealed class Chip8Emu {
+        //Chip-8 has 16 general purpose 8-bit registers,
+        private const int NumberOfGeneralPurposeRegisters = 16;
+        //Chip-8 allows for up to 16 levels of nested subroutines.
+        private const int MaxStackDepth = 16;
+        //The Chip-8 language is capable of accessing up to 4KB (4,096 bytes) of RAM, from location 0x000 (0) to 0xFFF (4095).
+        private const int RAMSizeInBytes = 4096;
+        //The first 512 bytes, from 0x000 to 0x1FF, are where the original interpreter was located, and should not be used by programs.
+        private const short ProgramStart = 512;
+        private const short ETI660ProgramStart = 1536;
 
-        public void EmulateStep()
-        {
+        //Registers         
+        private byte[] registers = new byte[NumberOfGeneralPurposeRegisters];
+        //There is also a 16-bit register called I. This register is generally used to store memory addresses, so only the lowest (rightmost) 12 bits are usually used.
+        private short i_register = new short();
+
+        private byte delay_timer = new byte();
+        private byte sound_timer = new byte();
+        private byte stack_pointer = new byte();
+        private short[] stack = new short[MaxStackDepth];
+        private short program_counter = new short();
+
+        private byte[] memory = new byte[RAMSizeInBytes];
+
+        private bool done = false;
+
+        public int Run(byte[] rom) {
+            //determine which speed to run rom at
+            //determine if ETI 660 computer
+            Load(rom);
+            program_counter = ProgramStart;
+            while (!done) {
+                EmulateStep();
+            }
+            return 0;
+        }
+
+        private void Load(byte[] rom) {
+            Buffer.BlockCopy(rom, 0, memory, ProgramStart, rom.Length);
+        }
+
+        private void EmulateStep() {
             //process current instuction
             //decrement counters
             //increment Program counter
---
+++ chip8core/RegisterNames.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chip8core
+{
+    enum RegisterNames
+    {
+        V0 = 0x0,
+        V1 = 0x1,
+        V2 = 0x2,
+        V3 = 0x3,
+        V4 = 0x4,
+        V5 = 0x5,
+        V6 = 0x6,
+        V7 = 0x7,
+        V8 = 0x8,
+        V9 = 0x9,
+        VA = 0xA,
+        VB = 0xB,
+        VC = 0xC,
+        VD = 0xD,
+        VE = 0xE,
+        VF = 0xF
+    }
+}
