16842fc60565ca2479a030532308952a32372b5a
Richard Nordström <croczter@gmail.com>
2018-12-28 18:01:04 +0100

    Started building a enum-to-delegate table

--- chip8core/Chip8Emu.cs
+++ chip8core/Chip8Emu.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Chip8Core {
     public sealed class Chip8Emu {
@@ -11,11 +12,13 @@ namespace Chip8Core {
         //The first 512 bytes, from 0x000 to 0x1FF, are where the original interpreter was located, and should not be used by programs.
         private const ushort ProgramStart = 512;
         private const ushort ETI660ProgramStart = 1536;
-
+        //The original implementation of the Chip-8 language used a 64x32-pixel monochrome display
+        private const ushort DisplayLineHeight = 32;
         //Registers         
         private byte[] registers = new byte[NumberOfGeneralPurposeRegisters];
         //There is also a 16-bit register called I. This register is generally used to store memory addresses, so only the lowest (rightmost) 12 bits are usually used.
         private short i_register = new short();
+        private IDictionary<InstructionType, Action<ushort>> InstructionTable;
 
         private byte delay_timer = new byte();
         private byte sound_timer = new byte();
@@ -24,8 +27,42 @@ namespace Chip8Core {
         private ushort program_counter = new ushort();
 
         private byte[] memory = new byte[RAMSizeInBytes];
+        private UInt64[] display = new UInt64[DisplayLineHeight];
 
         private bool done = false;
+
+        public Chip8Emu() {
+            InstructionTable
+        }
+
+        private IDictionary<InstructionType, Action<ushort>> GetMethodTable() {
+            return new Dictionary<InstructionType, Action<ushort>> {
+                {InstructionType.SYS_addr, (arg) => SysAddr(arg) },
+                {InstructionType.CLS, (arg) => ClearScreen(arg) },
+                {InstructionType.RET, (arg) => ReturnFromSubRoutine(arg) },
+                {InstructionType.JP_addr, (arg) => Jump(arg) },
+                {InstructionType.CALL_addr, (arg) => Call(arg) },
+                {InstructionType.SE_Vx_byte, (arg) => SkipIfEqual(arg) },
+                {InstructionType.SNE_Vx_byte, (arg) => SkipIfNotEqual(arg) },
+                {InstructionType.SE_Vx_Vy, (arg) => SkipIfEqualRegisters(arg) },
+                {InstructionType.LD_Vx_byte, (arg) => LoadByte(arg) },
+                {InstructionType.ADD_Vx_byte, (arg) => AddByte(arg) },
+                {InstructionType.LD_Vx_Vy, (arg) => LoadRegisters(arg) },
+                {InstructionType.OR_Vx_Vy, (arg) => OrRegisters(arg) },
+                {InstructionType.AND_Vx_Vy, (arg) => AndRegisters(arg) },
+                {InstructionType.XOR_Vx_Vy, (arg) => XorRegisters(arg) },
+                {InstructionType.ADD_Vx_Vy, (arg) => AddRegisters(arg) },
+                {InstructionType.SUB_Vx_Vy, (arg) => SubYFromXRegisters(arg) },
+                {InstructionType.SHR_Vx_Vy, (arg) => ShrRegisters(arg) },
+                {InstructionType.SUBN_Vx_Vy, (arg) => SubXFromYRegisters(arg) },
+                {InstructionType.SHL_Vx_Vy, (arg) => ShlRegisters(arg) },
+                {InstructionType.SE_Vx_Vy, (arg) => SkipIfEqualXY(arg) },
+                {InstructionType.SE_Vx_Vy, (arg) => SkipIfEqualXY(arg) },
+                {InstructionType.SE_Vx_Vy, (arg) => SkipIfEqualXY(arg) },
+                {InstructionType.SE_Vx_Vy, (arg) => SkipIfEqualXY(arg) },
+
+             };
+        }
 
         public int Run(byte[] rom) {
             //determine which speed to run rom at
@@ -52,7 +89,30 @@ namespace Chip8Core {
 
         private void ProcessInstruction() {
             Instruction instruction = GetInstruction();
-           // DecodeInstruction(instruction);
+            DecodeInstruction(instruction);
+        }
+
+        private void DecodeInstruction(Instruction instruction) {
+            //todo: implementation
+        }
+
+        private void SysAddr(ushort arg) {
+            //ignore;
+        }
+
+        private void ClearScreen(ushort argument) {
+            display = new UInt64[DisplayLineHeight];
+        }
+
+        private void ReturnFromSubRoutine(ushort argument) {
+            //todo: implementation
+        }
+        private void Call(ushort arg) {
+            throw new NotImplementedException();
+        }
+
+        private void Jump(ushort arg) {
+            throw new NotImplementedException();
         }
 
         /// <summary>
